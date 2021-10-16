d92d64531c1ee29a7556a0aaeda4b57de7bb82ae
malatindez <53783400+malatindez@users.noreply.github.com>
2021-10-17 01:29:28 +0300

    Update Door.cs

--- Assets/scripts/Door.cs
+++ Assets/scripts/Door.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class Door : Interactable
 {
+    public GameObject CharacterObject;
+
+    Animator animator;
 
     void Start()
     {
-
+        animator = gameObject.GetComponent<Animator>();
     }
+
+    [ContextMenu("Interact")]
     public override void Interact()
     {
-
+        if (CharacterObject.transform.position.x > gameObject.transform.position.x)
+        {
+            gameObject.transform.localScale = new Vector3(-1, 1, 1);
+        }
+        animator.SetBool("isOpened", true);
     }
+
 
     void Update()
     {
-        
+
     }
 }
