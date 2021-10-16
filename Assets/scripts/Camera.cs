a3e54b644d9a512eb843c0688ce0d96cca0779d0
malatindez <53783400+malatindez@users.noreply.github.com>
2021-10-16 19:43:39 +0300

    add character movement on click

--- Assets/scripts/Camera.cs
+++ Assets/scripts/Camera.cs
@@ -1,37 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Camera : MonoBehaviour
+public class Camera : MonoBehaviour, IPointerClickHandler
 {
+    [SerializeField] private Character _character;
+
+
+    int clickCounter = 0;
+    float lastClickTime = 0;
+
     public Outline SelectedItem { get; private set; } = null;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        int clickCount = eventData.clickCount;
+        OnMultiClick(clickCount);
+    }
+
+    void OnMultiClick(int i)
+    {
+        if(i <= 0)
+        {
+            return;
+        }
+
+        Ray ray = GetComponent<UnityEngine.Camera>().ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.TryGetComponent<Item>(out Item item))
+        {
+            if (SelectedItem != null)
+            {
+                SelectedItem.enabled = false;
+            }
+            SelectedItem = item.GetComponent<Outline>();
+            SelectedItem.enabled = true;
+        }
+        else if (SelectedItem != null)
+        {
+            SelectedItem.enabled = false;
+        }
+
+        if (i >= 4)
+        {
+            _character.MovingSpeed = Character.FastRunning;
+        }
+        else if (i >= 2)
+        {
+            _character.MovingSpeed = Character.Running;
+        }
+        else
+        {
+            _character.MovingSpeed = Character.Walking;
+        }
+        _character.TargetPosition = hit.point;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        
+        if (Input.GetMouseButton(0))
         {
-            Ray ray = GetComponent<UnityEngine.Camera>().ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.TryGetComponent<Item>(out Item item))
+            var currentTime = Time.realtimeSinceStartup;
+            if (Input.GetMouseButtonDown(0))
             {
-                if (SelectedItem != null)
+                if (currentTime - lastClickTime > 0.2f)
                 {
-                    SelectedItem.enabled = false;
+                    clickCounter = 1;
                 }
-                SelectedItem = item.GetComponent<Outline>();
-                SelectedItem.enabled = true;
+                else
+                {
+                    clickCounter += 1;
+                }
             }
-            else if (SelectedItem != null)
-            {
-                SelectedItem.enabled = false;
-            }
-        } 
+            lastClickTime = currentTime;
+            OnMultiClick(clickCounter);
+        }
     }
 }
--- Assets/scripts/Character.cs
+++ Assets/scripts/Character.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 
 public enum MovingMode  
 { 
-    Walking, 
-    Running, 
-    FastRunning 
 }
 
 public class Character : MonoBehaviour
 {
-    MovingMode MovingMode { get; set; }
-    float WalkSpeed { get; set; } = 5;
-    float RunSpeed { get; set; } = 7.5f;
-    float FastRunSpeed { get; set; } = 10;
-
+    public const float Walking = 5;
+    public const float Running = 7.5f;
+    public const float FastRunning = 10;
+    public const float ReachDistance = 0.3f;
+    public float MovingSpeed { get; set; } = Walking;
+    public Vector2 TargetPosition { get; set; }
     // Start is called before the first frame update
     void Start()
     {
-        
+        TargetPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float speed = (MovingMode == MovingMode.Walking) ? WalkSpeed : (MovingMode == MovingMode.Running) ? RunSpeed : FastRunSpeed;
-        transform.Translate(new Vector3(horizontalInput, 0, 0) * speed * Time.deltaTime);
+        if (Mathf.Abs(TargetPosition.x - transform.position.x) > ReachDistance)
+        {
+            int wayToGo = transform.position.x > TargetPosition.x ? -1 : 1;
+            transform.Translate(new Vector3(wayToGo, 0, 0) * MovingSpeed * Time.deltaTime);
+        }
     }
 }
