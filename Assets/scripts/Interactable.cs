200db808701beaabb54a3fdecf2668115234be67
malatindez <53783400+malatindez@users.noreply.github.com>
2021-10-17 06:04:18 +0300

    Update Interactable.cs

--- Assets/scripts/Interactable.cs
+++ Assets/scripts/Interactable.cs
@@ -4,29 +4,43 @@ using UnityEngine;
 
 public class Interactable : SpriteOutline
 {
+    public class Interaction
+    {
+        // Side from which the character came from.
+        // False for left, True for right.
+        public bool RightSide { get; set; } = false;
+        public Interaction()
+        {
+        }
+
+        public Interaction(bool rightSide)
+        {
+            this.RightSide = rightSide;
+        }
+    }
+    
     [SerializeField] protected float ReachDistance = 0.1f;
+
+    public Floor Floor;
     public float GetReachDistance()
     {
         return ReachDistance;
     }
 
     // Start is called before the first frame update
-    void Start()
+    protected void Start()
     {
         if (ReachDistance <= 0)
         {
             throw new System.ArgumentException("ReachDistance cannot be less or equal to zero!");
         }
         base.Start();
+        Floor = gameObject.transform.parent.parent.GetComponent<Floor>();
     }
 
 
-    public virtual void Interact()
+    public virtual void Interact(Interaction i)
     {
 
-    }
-    void Update()
-    {
-        base.Update();
     }
 }
