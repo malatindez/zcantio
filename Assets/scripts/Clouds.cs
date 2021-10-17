0b4693b6e6cde884f546cc07c97782efdcf51fa1
BerezkovN <nikita.berezkof@gmail.com>
2021-10-17 18:09:55 +0300

    Audio

--- Assets/scripts/Clouds.cs
+++ Assets/scripts/Clouds.cs
@@ -9,5 +9,8 @@ public class Clouds : MonoBehaviour
     void Update()
     {
         gameObject.transform.Translate(Vector3.left * 0.01f);
+        if (gameObject.transform.position.x < -40)
+            gameObject.transform.position = new Vector3(0, gameObject.transform.position.y, gameObject.transform.position.z);
+
     }
 }
