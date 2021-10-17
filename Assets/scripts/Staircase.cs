58621679b3c9258355ac92bed6bfc18d3ad06d3f
malatindez <53783400+malatindez@users.noreply.github.com>
2021-10-17 06:04:20 +0300

    Update Staircase.cs

--- Assets/scripts/Staircase.cs
+++ Assets/scripts/Staircase.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class Staircase : Interactable
 {
+    [SerializeField] Floor TargetFloor;
+    [SerializeField] private Character _character;
+    [SerializeField] private Vector2 offset;
+    [SerializeField] private bool setPosition;
+
     // Start is called before the first frame update
-    void Start()
+    new void Start()
     {
+        if(TargetFloor == null)
+        {
+            throw new System.ArgumentException("SerializeField TargetFloor cannot be null!");
+        }
         base.Start();
+    }
+    public override void Interact(Interaction i)
+    {
+        _character.ChangeFloor(TargetFloor, offset, setPosition);
     }
 
 }
