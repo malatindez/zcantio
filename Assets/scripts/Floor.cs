2566f758a62974ca017dd1ed33d9551f71c32613
malatindez <53783400+malatindez@users.noreply.github.com>
2021-10-17 06:02:20 +0300

    Create Floor.cs

---
+++ Assets/scripts/Floor.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+    private List<Transform> foregroundObjects;
+    void Start()
+    {
+        foregroundObjects = new List<Transform>();
+        transform.hasChanged = true;
+    }
+
+    public bool checkForObstacles(float position, float target, Transform skip = null)
+    {
+        float temp = target - position;
+        foreach(Transform foregroundObject in foregroundObjects)
+        {
+            bool flagA = foregroundObject.position.x - position > 0;
+            bool flagB = foregroundObject.position.x - position < temp;
+            if ((skip == null || skip != foregroundObject) &&
+                ((temp > 0 && flagA && flagB) ||
+                (temp <= 0 && !flagA && !flagB ))
+                )
+                   return true;
+        }
+        return false;
+    }
+    private void Update()
+    {
+        if (transform.hasChanged)
+        {
+            foregroundObjects.Clear();
+
+            foreach (Transform transform in transform.Find("Foreground").transform)
+            {
+                foregroundObjects.Add(transform);
+            }
+            transform.hasChanged = false;
+        }
+    }
+}
